2f5b59b27e29021911fd8cb09e7750955ccb5f87
Lucast34 <lucast34t@gmail.com>
2025-03-25 11:27:49 -0300

    DQL

---
+++ sql/DML/DQL.sql
@@ -0,0 +1,13 @@
+use db_barbearia;
+
+# 1
+select * from tb_servicos;
+
+#2
+select nome from tb_servicos where id= 3;
+
+#3
+select * from tb_servicos where valor > 30.00;
+
+#4
+select nome as servico from tb_servicos; 
