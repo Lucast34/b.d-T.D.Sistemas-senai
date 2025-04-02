4776a8ef83e07a13b091e155439d2a0b66284024
Lucast34 <lucast34t@gmail.com>
2025-04-02 10:22:07 -0300

    engenaria_reversa

--- sql/desafios/desafio01/sql/desafio02.sql
+++ sql/desafios/desafio01/sql/desafio02.sql
@@ -3,7 +3,7 @@ use db_ruido;
 insert into tb_cidade(sigla) value('tag-nor'),('tag-sul'),('ceil'),('sam-nor'),('sam-sul'),('pla-pil'),('nuc-ban');
 
 
-insert into tb_cruzamento(nome, ruido) values
+insert into tb_cruzamento(sigla, ruido) values
 ('br-01',85),
 ('br-02',92),
 ('br-03',95),
@@ -22,16 +22,16 @@ insert into tb_cruzamento(nome, ruido) values
 ('br-16',112);
 
 -- min
-select nome, min(ruido)as min_ruido from tb_cruzamento;
+select sigla, min(ruido)as min_ruido from tb_cruzamento;
 
 -- avg
-select nome, avg(ruido)as avg_ruidoo from tb_cruzamento;
+select sigla, avg(ruido)as avg_ruidoo from tb_cruzamento;
 
 -- max
-select nome, max(ruido)as max_rudio from tb_cruzamento;
+select sigla, max(ruido)as max_rudio from tb_cruzamento;
 
 -- count
-select nome, count(ruido)as count_rudio from tb_cruzamento;
+select sigla, count(ruido)as count_rudio from tb_cruzamento;
 
 -- sum
-select nome, sum(ruido)as sum_rudio from tb_cruzamento;
+select sigla, sum(ruido)as sum_rudio from tb_cruzamento;
